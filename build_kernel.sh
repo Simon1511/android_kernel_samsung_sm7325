b45abea5019e6afadc4103ad236a8323c507dd54
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-07-06 10:31:00 +0200

    Initial build script
    Signed-off-by: Simon1511 <simon2002.schoenmackers@gmail.com>
    Change-Id: I1292fa24f0396bca120e3d2ed38616c4a302849d

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 export ARCH=arm64
-mkdir out
 
-BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
+# GCC from https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
+BUILD_CROSS_COMPILE=../toolchain/gcc/bin/aarch64-linux-android-
+
+# SD-Clang 10 from https://github.com/xiangfeidexiaohuo/Snapdragon-LLVM
+KERNEL_LLVM_BIN=../toolchain/sdclang/bin/clang
+
 CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y vendor/a52sxq_eur_open_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_SECTION_MISMATCH_WARN_ONLY=y
- 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+make -j$(nproc --all) ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/a52sxq_eur_open_defconfig
+make -j$(nproc --all) ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
